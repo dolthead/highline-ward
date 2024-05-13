e484728a8bbe8f5a838b8233bd31e99a01134c2d
Todd H <github@simplybest.com>
2024-05-12 22:05:40 -0600

    mothers day

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup>
-  import { IonContent, IonPage } from "@ionic/vue";
-  import HymnRow from "./HymnRow.vue";
-  import { getHymn } from '@/data/hymns';
+import { IonContent, IonPage } from "@ionic/vue";
+import HymnRow from "./HymnRow.vue";
+import { getHymn } from '@/data/hymns';
 
-  const nextSunday = new Date();
-  nextSunday.setDate(nextSunday.getDate() - nextSunday.getDay() + (nextSunday.getDay() >= 2 ? 7 : 0)); // 2 = TUESDAY
-  const sundate = nextSunday.toLocaleDateString(['en-US'], {month: 'long', day: '2-digit', year: 'numeric'});
-  const isDecember = nextSunday.getMonth() === 11;
-  const image = isDecember
-    ? { path: '/assets/maryjesus.jpeg', alt: 'Mary holding baby Jesus' }
-    : { path: '/assets/Christus.png', alt: 'Statue of Christ' };
+const nextSunday = new Date();
+nextSunday.setDate(nextSunday.getDate() - nextSunday.getDay() + (nextSunday.getDay() >= 2 ? 7 : 0)); // 2 = TUESDAY
+const sundate = nextSunday.toLocaleDateString(['en-US'], { month: 'long', day: '2-digit', year: 'numeric' });
+const isDecember = nextSunday.getMonth() === 11;
+const image = isDecember || true
+  ? { path: '/assets/maryjesus.jpeg', alt: 'Mary holding baby Jesus' }
+  : { path: '/assets/Christus.png', alt: 'Statue of Christ' };
+  // : { path: '/assets/ChristPraying.png', alt: 'Christ Kneeling in Prayer' };
 </script>
 
 <template>
@@ -23,67 +24,55 @@
         <div>{{ sundate }}</div>
         <div class="table">
           <div></div>
-          <!-- <div><span>Presiding</span><span>President Matthew Hinton</span></div> -->
           <div><span>Presiding</span><span>Bishop Glen Ostler</span></div>
           <!-- <div><span>Presiding</span><span>Brother Kayle Smith</span></div> -->
-          
-          <div><span>Conducting</span><span>Brother Brent Oakeson</span></div>
 
-          <div><span>Music Director</span><span>Sister Carina Sackley</span></div>
-          <!-- <div><span>Music Director</span><span>Sister LeAnn Ostler</span></div> -->
+          <div><span>Conducting</span><span>Brother Kayle Smith</span></div>
+
+          <!-- <div><span>Music Director</span><span>Sister Carina Sackley</span></div> -->
+          <div><span>Music Director</span><span>Sister LeAnn Ostler</span></div>
 
           <!-- <div><span>Organist</span><span>Sister Kaylene DeMasi</span></div> -->
           <div><span>Organist</span><span>Sister Shellie Vanfleet</span></div>
-          <!-- <div><span>Organist</span><span>Brother Vince Butterfield</span></div> -->
 
           <div></div>
-          <hymn-row :hymn="getHymn(72)">Opening Hymn</hymn-row>
-          <div><span>Invocation</span><span>Brother Ian Hopper</span></div>
+          <hymn-row :hymn="getHymn(52)">Opening Hymn</hymn-row>
+          <div><span>Invocation</span><span>Sister Ericka Eggleston</span></div>
           <div></div>
 
           <div>
             <span>Ward & Stake Items</span>
           </div>
           <div></div>
-          <!-- <div><em>The video feed for those at home will resume after the sacrament</em></div>
-          <div></div> -->
 
-          <hymn-row :hymn="getHymn(146)">Sacrament Hymn</hymn-row>
+          <hymn-row :hymn="getHymn(185)">Sacrament Hymn</hymn-row>
           <div><strong>Administration of the Lord's Sacrament</strong></div>
           <div></div>
 
           <!-- <div><span>Testimonies</span></div>
           <div></div> -->
 
-          <div><span>Youth Speaker</span><span>Taryn Serna</span></div>
-          <!-- <div class="smaller"><span></span><span>Stake Primary President</span></div> -->
+          <div><span>Speakers</span><span>Finn Kunz</span></div>
+          <div><span></span><span>Tallin Kunz</span></div>
           <div></div>
 
-          <div><span>Speaker</span><span>Brother Guy Keisel</span></div>
-          <!-- <div class="smaller"><span></span><span>Stake Aaronic Priesthood Camp Director</span></div> -->
-          <div></div>
-
-          <!-- <div><span>Speaker</span><span>Elder Kelton Smith</span></div>
-          <div class="smaller"><span></span><span>Leaving soon for the Kenya Nairobi Mission</span></div>
+          <!-- <hymn-row :hymn="getHymn(113)">Congregational Hymn</hymn-row>
           <div></div> -->
-
-          <hymn-row :hymn="getHymn(301)">Congregational Hymn</hymn-row>
-          <div></div>
-
-          <!-- <div><span>Primary Number</span><span>Faith</span></div>
-          <div class="smaller"><em>Dir. by Sister Aleesa Hale, Accomp. by Sister Janet Deveraux</em></div>
-          <div></div> -->
-          <!-- <div><span>Ward Choir</span><span>Great Things and Small Things</span></div>
+          <!-- <div><span>Ward Choir</span><span>Cast Your Burden on the Lord</span></div>
           <div class="smaller"><em>Dir. by Brother Michael Lambert, Accomp. by Sister Shellie Vanfleet</em></div>
           <div></div> -->
-
-          <div><span>Speaker</span><span>Brother Vince Butterfield</span></div>
-          <!-- <div class="smaller"><span></span><span>Stake Primary President</span></div> -->
+          <div><span>Primary & Young Women Musical Number</span><span>More Than Enough</span></div>
+          <div class="smaller"><em>Dir. by Sister Aleesa Hale, Accomp. by Sister Janet Deveraux</em></div>
           <div></div>
 
-          <hymn-row :hymn="getHymn(241)">Closing Hymn</hymn-row>
-          <div><span>Benediction</span><span>Sister Marie Hopper</span></div>
-          
+          <div><span>Speakers</span><span>Case Kunz</span></div>
+          <div><span></span><span>Mike Kunz</span></div>
+          <div class="smaller"><span></span><span>Stake High Councilor</span></div>
+          <div></div>
+
+          <hymn-row :hymn="getHymn(86)">Closing Hymn</hymn-row>
+          <div><span>Benediction</span><span>Brother Steve Eggleston</span></div>
+
           <!-- <div></div>
           <div></div>
           <div><em>Linger longer today: visiting and snacks<br>in the pavilion after church.</em></div> -->
@@ -102,11 +91,11 @@ I will find my own sacred grove.
         <div class="announcements">
           <hr />
 
-          <!-- <div>
-            ♡ <em>Merry Christmas!</em> ♡
+          <div>
+            ♡ <em>Happy Mothers Day!</em> ♡
           </div>
-          <div></div>  -->
-          
+          <div></div> 
+
           <!-- <div>
             Sign up for tithing declarations with Bishop Ostler on
             <a href="https://docs.google.com/document/d/1oChAm94PKdQl9Gauts96YNsDHyNCNSZmnCxt43_Aujw/edit?usp=sharing">this sign-up sheet</a>.
@@ -123,11 +112,12 @@ I will find my own sacred grove.
           </div>
           <div></div>
           <div>
-            The <a href="https://highlineward.org/sacrament">live stream for Sacrament meeting</a> now requires a password, which can be requested from Bishop Ostler.
+            The <a href="https://highlineward.org/sacrament">live stream for Sacrament meeting</a> requires a
+            password, which can be requested from Bishop Ostler.
           </div>
           <div></div>
           <div>
-            Easily share this program by 
+            Easily share this program by
             <a href="sms:?&body=https://highline-ward.web.app">sending a text message</a>,
             or by having the person point their phone camera at this QR code:
           </div>
@@ -135,7 +125,8 @@ I will find my own sacred grove.
           <img src="/assets/qrcode.png" alt="QR code for this church program" />
           <div></div>
           <div class="smaller">
-            This is not an official website of <a href="https://www.churchofjesuschrist.org/">The Church of Jesus Christ of Latter-day Saints</a>.
+            This is not an official website of <a href="https://www.churchofjesuschrist.org/">The Church of Jesus Christ
+              of Latter-day Saints</a>.
           </div>
         </div>
       </div>
@@ -144,67 +135,82 @@ I will find my own sacred grove.
 </template>
 
 <style scoped>
+.container {
+  margin: 20px;
+  text-align: center;
+  font-size: 3.0vw;
+}
+
+@media print {
   .container {
-    margin: 20px;
-    text-align: center;
-    font-size: 3.0vw;
+    font-size: 14pt;
+    max-width: 75%;
+    margin: 0 auto;
   }
-  @media print {
-    .container {
-      font-size: 14pt;
-      max-width: 75%;
-      margin: 0 auto;
-    }
-    .container img:nth-child(1), 
-    .announcements {
-      display: none;
-    }
+
+  .container img:nth-child(1),
+  .announcements {
+    display: none;
   }
-  img {
-    width: 30vw;
-    max-width: 250px;
-  }
-  div {
-    min-height: 1.6em;
-  }
-  .title {
-    text-align: center;
-    font-weight: bold;
-  }
-  div.table div {
-    width: 100%;
-  }
-  div.table span:last-child {
-    float: right;
-  }
-  div.table span:first-child {
-    clear: both;
-    float: left;
-  }
-  hr {
-    border-top: 1px navy solid;
-    margin: 2em 0;
-  }
-  pre {
-    font-family: Roboto, "Helvetica Neue", sans-serif;
-  }
-  .smaller {
-    font-size: 2.6vw;
-    margin-top: -3px;
-    margin-bottom: 3px;
-  }
-  .program div {
-    margin: 6px;
-  }
-  .clean-button {
-    background: none;
-    border: none;
-    border-radius: 6px;
-    padding: 6px;
-    margin-top: 6px;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
-  }
+}
+
+img {
+  width: 30vw;
+  /* width: 40vw; */
+  max-width: 250px;
+}
+
+div {
+  min-height: 1.6em;
+}
+
+.title {
+  text-align: center;
+  font-weight: bold;
+}
+
+div.table div {
+  width: 100%;
+  clear: both;
+}
+
+div.table span:last-child {
+  float: right;
+}
+
+div.table span:first-child {
+  clear: both;
+  float: left;
+}
+
+hr {
+  border-top: 1px navy solid;
+  margin: 2em 0;
+}
+
+pre {
+  font-family: Roboto, "Helvetica Neue", sans-serif;
+}
+
+.smaller {
+  font-size: 2.6vw;
+  margin-top: -1px;
+  margin-bottom: 3px;
+}
+
+.program div {
+  margin: 6px;
+}
+
+.clean-button {
+  background: none;
+  border: none;
+  border-radius: 6px;
+  padding: 6px;
+  margin-top: 6px;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
+}
 </style>
