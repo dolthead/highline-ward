c8df5ff3a40a8050887f8bf069afed7fd4b684d8
Todd H <github@simplybest.com>
2025-01-13 18:02:54 -0700

    update to latest packages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.simplybest.highlineward',
+  appName: 'Highline Ward',
+  webDir: 'dist',
+};
+
+export default config;
