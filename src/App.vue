4aec22720c9afceb59e14238ec9a3ad4740da3b4
Todd H <github@simplybest.com>
2025-11-12 18:27:10 -0700

    do forward

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <!-- <ion-router-outlet /> -->
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+// import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    // IonRouterOutlet
   }
 });
+
+// forward user to https://docs.google.com/document/d/18TmnGSwRE5N31qJU0qVItQTTnFHnWz6HKm6WSBFR9qk/edit?usp=sharing
+window.location.href = 'https://docs.google.com/document/d/18TmnGSwRE5N31qJU0qVItQTTnFHnWz6HKm6WSBFR9qk/edit?usp=sharing';
 </script>
